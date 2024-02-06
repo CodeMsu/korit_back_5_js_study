c8bc0c8ee51dd0638f8031bd0e2ac10effea7dc7
dawdawhhsh <ddukbai98@gmail.com>
2024-02-06 17:48:00 +0900

    javascript

---
+++ hello.js
@@ -0,0 +1,15 @@
+// << 자바스크립트 >>
+// 베이스 문법 -> 자바
+// 타입리스
+
+var num = 10;
+num = "안녕";
+console.log(typeof(10.0));
+console.log(typeof("10"));
+console.log(typeof(true));
+var n;
+console.log(typeof(n));
+console.log(n);
+n = null;
+console.log(typeof(n));
+console.log(n);
